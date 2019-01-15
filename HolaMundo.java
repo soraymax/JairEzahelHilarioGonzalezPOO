779f707b0400f620648a47b05db1d945982c9a03
soraymax <tecate_pelas@hotmail.com>
2019-01-15 13:12:56 -0600

    VER NOMBRE

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HolaMundo
+{
+	public static void main(String[]args)
+		{
+		String NOMBRE;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Escribe el nombre");
+		NOMBRE = scanner.next();
+		System.out.println("Hola "+NOMBRE);
+		}
+	
+}
